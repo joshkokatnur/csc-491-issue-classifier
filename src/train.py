1161c16a4bb3618dbb8842d1851f41921e270140
joshkokatnur <jkokatnur@gmail.com>
2025-10-26 12:53:59 -0400

    Setup wandb

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from cProfile import run
-#import wandb
+import wandb
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
@@ -25,11 +25,11 @@ def train(args):
     running_loss = 0.0
 
     # Setting Summary metrics for epochwise evaluation
-    #wandb.run.summary["best_F1"] = 0.0
+    wandb.run.summary["best_F1"] = 0.0
 
     # Best epoch is epoch where we get the best F1 score
-    #wandb.run.summary["best_epoch"] = 0
-    #wandb.run.summary["best_step"] = 0
+    wandb.run.summary["best_epoch"] = 0
+    wandb.run.summary["best_step"] = 0
 
     output_model_name = args.SAVED_MODELS_DIR + args.MODEL_NAME +"_classifier"+ args.DATASET_SUFFIX+ "_" + 'best' +  ".bin"
 
@@ -57,26 +57,26 @@ def train(args):
                 # Eval and Log
                 if idx % args.update_freq == 0 and idx != 0:
                     print("[INFO] Epoch: {} Loss : {}".format(epoch,running_loss/args.update_freq))
-                    #wandb.log({"Loss": running_loss/args.update_freq})
+                    wandb.log({"Loss": running_loss/args.update_freq})
                     running_loss = 0.0
 
                     F1 = evaluate_model(model, args, split = 'val')
                     model.train() #Necessary after calling eval above
-                    #wandb.log({'F1': F1}) # Log F1 score for model trained upto this step
+                    wandb.log({'F1': F1}) # Log F1 score for model trained upto this step
                     print("[INFO] Model evaluated and scores logged to server")
-                    '''if F1 > wandb.run.summary["best_F1"]:
+                    if F1 > wandb.run.summary["best_F1"]:
                         save(model,optim,output_model_name)
                         wandb.run.summary["best_F1"] = F1
                         wandb.run.summary["best_epoch"] = epoch
                         wandb.run.summary["best_step"] = idx
                         print(f"[INFO] Model saved for epoch: {epoch}.{idx}")
                     else:
-                        wandb.run.summary["best_F1"] = wandb.run.summary["best_F1"]'''
+                        wandb.run.summary["best_F1"] = wandb.run.summary["best_F1"]
 
     # Generate test scores
     load(model, output_model_name)
     F1 = evaluate_model(model, args, split = 'test')
-    #wandb.run.summary["test_F1"] = F1
+    wandb.run.summary["test_F1"] = F1
     print("[INFO] Model evaluated on test set and scores logged to server")
 
 
@@ -94,5 +94,5 @@ if __name__ == "__main__":
     args, logging_args = get_arguments()
     set_random_seed(args.seed, is_cuda = args.device == torch.device('cuda'))
     create_modified_dataset(args)
-    # wandb.init(project=args.project, entity="iisc1", config = logging_args)
+    wandb.init(project="NLBSE 2022 Issue Classification", entity="csc491-group", config = logging_args)
     train(args)
